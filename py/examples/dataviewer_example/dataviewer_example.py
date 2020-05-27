0aecd830e5691041d924d654ed2acaf72a8fe953
Nitin Nair <nitin7nair@gmail.com>
2020-05-27 15:02:05 -0400

    viewer example update

--- py/examples/dataviewer_example/dataviewer_example.py
+++ py/examples/dataviewer_example/dataviewer_example.py
@@ -6,20 +6,13 @@ Created on Thu Aug  15 3:06:23 2019
 @author: Nitin
 """
 
-# import emotibit.datasyncer as syncer
-# import matplotlib.pyplot as plt
-# import locale
-# import numpy
-# import emotibit.flexcompparser as flexcomp
 import sys
 import emotibit.dataviewer as dataviewer
-# sys.path.append('/Users/nitin/GoogleDrive/MacBookPro/DocumentsRemote/CFLunpaid/Emotibit/dataAnalysis/EmotiBit_Biometric_Lib/py')
-# my_syncer = syncer.DataSyncer()
 
-# Load EmotiBit data - specify file name and path
-
-file_dir0 = r"C:\Users\nitin\Documents\EmotiBit\DataAnalysis\controlledTest\2019-12-10_11-55-54-038975\dataParsed"
-file_base = r"2019-12-10_11-55-54-038975"
+# specify the location of the folder which contains the parsed data
+file_dir = r"path/to/parsed/data"  # Ex. C:\Users\dev\data
+# specify the base file name of teh parsed data
+file_base = r"base-file-name"  # 2019-12-10_11-55-54-038975
 
 # arguments for command line
 # usernote_toggle False hide_dc EA,ER
@@ -35,4 +28,4 @@ for i, argument in enumerate(sys.argv[1:]):
             if sys.argv[i+2] == "False":
                 userNote_toggle = False
 
-analysis = dataviewer.DataViewer(file_dir0, file_base, hide_DC_tags, userNote_toggle)
+analysis = dataviewer.DataViewer(file_dir, file_base, hide_DC_tags, userNote_toggle)
