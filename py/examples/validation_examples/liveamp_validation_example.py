ce24a58f42252fec7e06d934b29f1280e3936357
Sean Montgomery <produceconsumerobot@gmail.com>
2022-09-30 05:11:33 -0700

    Added twin plots to reg plots

--- py/examples/validation_examples/liveamp_validation_example.py
+++ py/examples/validation_examples/liveamp_validation_example.py
@@ -35,27 +35,28 @@ fs_eb["AZ"] = 25
 fs_eb["PI"] = 100
 
 # ECG + EDA + Accel files
-liveamp_file_name = r'C:\priv\gd\Dropbox\CFL\EmotiBit\EmotiBit CFL Share\Science\data\measurement sensors\2022-09-28\2022-09-28_09-53.xdf'
-file_dir = r"C:\priv\gd\Dropbox\CFL\EmotiBit\EmotiBit CFL Share\Science\data\measurement sensors\2022-09-28"
-file_base = r"2022-09-28_09-52-10-439680"
-liveamp_ind = {}
-liveamp_ind["EA"] = [0, 0] 
-liveamp_ind["AX"] = [0, 1] 
-liveamp_ind["AY"] = [0, 2] 
-liveamp_ind["AZ"] = [0, 3] 
-liveamp_ind["ECG"] = [1, 0] 
-trim_durations = [30, 30]
+# liveamp_file_name = r'C:\priv\gd\Dropbox\CFL\EmotiBit\EmotiBit CFL Share\Science\data\measurement sensors\2022-09-28\2022-09-28_09-53.xdf'
+# file_dir = r"C:\priv\gd\Dropbox\CFL\EmotiBit\EmotiBit CFL Share\Science\data\measurement sensors\2022-09-28"
+# file_base = r"2022-09-28_09-52-10-439680"
+# liveamp_ind = {}
+# liveamp_ind["EA"] = [0, 0] 
+# liveamp_ind["AX"] = [0, 1] 
+# liveamp_ind["AY"] = [0, 2] 
+# liveamp_ind["AZ"] = [0, 3] 
+# liveamp_ind["ECG"] = [1, 0] 
 
 # EDA + Accel files
-# liveamp_file_name = r'C:\priv\gd\Dropbox\CFL\EmotiBit\EmotiBit CFL Share\Science\data\measurement sensors\2022-09-21\20220921-2336.xdf'
-# file_dir = r"C:\priv\gd\Dropbox\CFL\EmotiBit\EmotiBit CFL Share\Science\data\measurement sensors\2022-09-21"
-# file_base = r"2022-09-21_23-34-20-381606"
-# liveamp_ind = {}
-# liveamp_ind["EA"] = 0 
-# liveamp_ind["AX"] = 2 
-# liveamp_ind["AY"] = 3 
-# liveamp_ind["AZ"] = 4 
+liveamp_file_name = r'C:\priv\gd\Dropbox\CFL\EmotiBit\EmotiBit CFL Share\Science\data\measurement sensors\2022-09-21\20220921-2336.xdf'
+file_dir = r"C:\priv\gd\Dropbox\CFL\EmotiBit\EmotiBit CFL Share\Science\data\measurement sensors\2022-09-21"
+file_base = r"2022-09-21_23-34-20-381606"
+liveamp_ind = {}
+liveamp_ind["EA"] = [0, 0] 
+liveamp_ind["AX"] = [0, 2] 
+liveamp_ind["AY"] = [0, 3] 
+liveamp_ind["AZ"] = [0, 4]
 
+
+trim_durations = [30, 30]
 simple_xdf_plot = 0
 
 timestamp_id = "LslMarkerSourceTimestamp"
@@ -97,18 +98,37 @@ def regression_plots(x, y, title = "", xlabel = "", ylabel = "", ylim = []):
         
     plot_data_rs_la = scp.signal.resample(plot_data_la[shared_ind_la], n_resamp)
     plot_data_rs_eb = scp.signal.resample(plot_data_eb[shared_ind_eb], n_resamp)
-       
+    
     # Plot resampled overlapping data
     fig, ax = plt.subplots(2)
+    if (("ylim" in x.keys()) and ("ylim" in y.keys())): # twin plot
+        # setup twin axes
+        ax2 = ax[0].twinx()
+    else:
+        ax2 = ax[0]
+       
     ax[0].plot(plot_timestamp_rs, plot_data_rs_la, color="red", label=y["label"])
-    ax[0].plot(plot_timestamp_rs, plot_data_rs_eb, color="black", label=x["label"])
+    if ("ylim" in x.keys()):
+        ax[0].set_ylim(y["ylim"])
+    ax2.plot(plot_timestamp_rs, plot_data_rs_eb, color="black", label=x["label"])
+    if ("ylim" in y.keys()):
+        ax2.set_ylim(x["ylim"])
     ax[0].legend()
+    ax2.legend(loc='lower right')
     ax[0].set_xlabel(xlabel)
     ax[0].set_ylabel(ylabel)
     if (len(ylim) == 2):
         ax[0].set_ylim(ylim)
+        ax2.set_ylim(ylim)
+    plt.show()
     
-    
+    # ax1.plot(plot_timestamps_la, plot_data_la, color="red")
+    # ax1.set_ylabel("Brain Products", color="red", fontsize=14)
+    # ax2.plot(plot_timestamps_eb, plot_data_eb, color="black")
+    # ax2.set_ylabel("EmotiBit",  color="black",  fontsize=14)
+    # ax1.set_xlabel("Time (seconds)")
+    # plt.show()
+       
     # Remove outliers
     lims = [stats.mean(plot_data_rs_eb) - np.std(plot_data_rs_eb)*3,
             stats.mean(plot_data_rs_eb) + np.std(plot_data_rs_eb)*3]
@@ -158,7 +178,9 @@ if (simple_xdf_plot):
 
 #### Script Begin ####
 
-### Accelerometer ###
+#######################################################
+# Accelerometer 
+#######################################################
 
 # setup twin axes
 fig, ax1 = plt.subplots(1)
@@ -241,7 +263,7 @@ plot_timestamps_la = stream['time_stamps']
 plot_data_la = stream['time_series'][:, liveamp_ind[type_tag][1]] / 25000 # uS / 25000 mV for Brain Products
 plot_data_la, plot_timestamps_la = trim_data(plot_data_la, plot_timestamps_la, fs_la[type_tag], trim_durations) 
 plt.plot(plot_timestamps_la, plot_data_la, color="red")
-ax1.set_ylabel("Brain Products", color="red", fontsize=14)
+ax2.set_ylabel("Brain Products", color="red", fontsize=14)
 
 # Plot EmotiBit EDA 
 type_tag = "EA"    
@@ -257,7 +279,7 @@ plot_data_eb = data[t][type_tag]
 # Trim beginning / end of data to remove junk & sync taps
 plot_data_eb, plot_timestamps_eb = trim_data(plot_data_eb, plot_timestamps_eb, fs_eb[type_tag], trim_durations) 
 plt.plot(plot_timestamps_eb, plot_data_eb, color="black")
-ax2.set_ylabel("EmotiBit",  color="black",  fontsize=14)
+ax1.set_ylabel("EmotiBit",  color="black",  fontsize=14)
 ax1.set_xlabel("Time (seconds)")
 plt.show()
 
@@ -265,11 +287,13 @@ reg_data_x = {}
 reg_data_x["label"] = "EmotiBit"
 reg_data_x["data"] = plot_data_eb
 reg_data_x["timestamps"] = plot_timestamps_eb
+reg_data_x["ylim"] = [0.4, 1.1]
 
 reg_data_y = {}
 reg_data_y["label"] = "Brain Products"
 reg_data_y["data"] = plot_data_la
 reg_data_y["timestamps"] = plot_timestamps_la
+reg_data_y["ylim"] = [3.5, 5.6]
 
 regression_plots(reg_data_x, reg_data_y, title="EDA", xlabel="Time (seconds)", ylabel="EDA (uSiemens)")
 
@@ -278,7 +302,9 @@ plot_data_eb = bt.band_filter(plot_data_eb, np.array(eda_bandpass), fs_eb[type_t
 plot_data_la = bt.band_filter(plot_data_la, np.array(eda_bandpass), fs_la[type_tag], order=4)
 # Remove filter artifact (3x duration of bandpass[0])
 reg_data_x["data"], reg_data_x["timestamps"] = trim_data(plot_data_eb, plot_timestamps_eb, fs_eb[type_tag], [4/eda_bandpass[0], 4/eda_bandpass[0]]) 
+reg_data_x["ylim"] = [-.15, .15]
 reg_data_y["data"], reg_data_y["timestamps"] = trim_data(plot_data_la, plot_timestamps_la, fs_la[type_tag], [4/eda_bandpass[0], 4/eda_bandpass[0]]) 
+reg_data_y["ylim"] = [-.75, .75]
 
 regression_plots(reg_data_x, reg_data_y, 
                  title="EDR (EDA Filtered {:.1f}".format(eda_bandpass[0]) + "-{:.1f}Hz)".format(eda_bandpass[1])
@@ -287,75 +313,75 @@ regression_plots(reg_data_x, reg_data_y,
 #######################################################
 # Heart Rate 
 #######################################################
-
-# setup twin axes
-fig, ax1 = plt.subplots()
-ax2 = ax1.twinx()
-# Brain Products 
-type_tag = "ECG"
-stream = liveamp_data[liveamp_ind[type_tag][0]]
-plot_timestamps_la = stream['time_stamps']
-plot_data_la = -stream['time_series'][:, liveamp_ind[type_tag][1]] / 25000 # uS / 25000 mV for Brain Products
-plot_data_la, plot_timestamps_la = trim_data(plot_data_la, plot_timestamps_la, fs_la[type_tag], trim_durations) 
-#plt.plot(plot_timestamps_la, plot_data_la, color="red")
-
-# Bandpass filter
-plot_data_la = bt.band_filter(plot_data_la, np.array(ecg_bandpass), fs_la[type_tag], order=4)
-plot_data_la, plot_timestamps_la = trim_data(plot_data_la, plot_timestamps_la, fs_la[type_tag], [5/ecg_bandpass[0], 5/ecg_bandpass[0]])
-ax1.plot(plot_timestamps_la, plot_data_la, color="red")
-ax1.set_ylabel("Brain Products (ECG)", color="red", fontsize=14)
-# Detect Peaks
-peaks_la, _ = scp.signal.find_peaks(plot_data_la, 0.015)
-ax1.plot(plot_timestamps_la[peaks_la], plot_data_la[peaks_la], "o", color="red")
-
-# EmotiBit 
-type_tag = "PI"    
-data = []   
-file_path = file_dir + '\\' + file_base + '\\' + file_base + '_' + type_tag + '.csv'
-print(file_path)
-data.append(pd.read_csv(file_path))
-t = 0
-plot_timestamps_eb = data[t][timestamp_id].to_numpy()
-plot_data_eb = -data[t][type_tag]
-plot_data_eb, plot_timestamps_eb = trim_data(plot_data_eb, plot_timestamps_eb, fs_eb[type_tag], trim_durations) 
-#plt.plot(plot_timestamps_eb, plot_data_eb, color="black")
-# Bandpass filter
-plot_data_eb = bt.band_filter(plot_data_eb, np.array(ppg_bandpass), fs_eb[type_tag], order=4)
-plot_data_eb, plot_timestamps_eb = trim_data(plot_data_eb, plot_timestamps_eb, fs_eb[type_tag], [5/ppg_bandpass[0], 5/ppg_bandpass[0]]) 
-ax2.plot(plot_timestamps_eb, plot_data_eb, color="black")
-# Detect Peaks
-peaks_eb, _ = scp.signal.find_peaks(plot_data_eb, 75, None, fs_eb[type_tag] / 2)
-ax2.plot(plot_timestamps_eb[peaks_eb], plot_data_eb[peaks_eb], "o", color="black")
-ax2.set_ylabel("EmotiBit (PPG)", color="black", fontsize=14)
-ax1.set_xlabel("Time (seconds)")
-plt.show()
-
-# setup twin axes
-# Calculate HR
-# Brain Products
-type_tag = "ECG"
-ibis_la = np.diff(peaks_la)/fs_la[type_tag]
-hr_la = 1 / ibis_la * 60
-plot_data_la = hr_la
-plot_timestamps_la = plot_timestamps_la[peaks_la[0:len(peaks_la)-1]]
-
-# EmotiBit
-type_tag = "PI" 
-ibis_eb = np.diff(peaks_eb)/fs_eb[type_tag]
-hr_eb = 1 / ibis_eb * 60
-plot_data_eb = hr_eb
-plot_timestamps_eb = plot_timestamps_eb[peaks_eb[0:len(peaks_eb)-1]]
-plt.show()
-
-reg_data_x = {}
-reg_data_x["label"] = "EmotiBit"
-reg_data_x["data"] = hr_eb
-reg_data_x["timestamps"] = plot_timestamps_eb
-
-reg_data_y = {}
-reg_data_y["label"] = "Brain Products"
-reg_data_y["data"] = hr_la
-reg_data_y["timestamps"] = plot_timestamps_la
-
-regression_plots(reg_data_x, reg_data_y, 
-                 title="Heart Rate", xlabel="Time (seconds)", ylabel="Heart Rate (BPM)")
+if (0):
+    # setup twin axes
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    # Brain Products 
+    type_tag = "ECG"
+    stream = liveamp_data[liveamp_ind[type_tag][0]]
+    plot_timestamps_la = stream['time_stamps']
+    plot_data_la = -stream['time_series'][:, liveamp_ind[type_tag][1]] / 25000 # uS / 25000 mV for Brain Products
+    plot_data_la, plot_timestamps_la = trim_data(plot_data_la, plot_timestamps_la, fs_la[type_tag], trim_durations) 
+    #plt.plot(plot_timestamps_la, plot_data_la, color="red")
+    
+    # Bandpass filter
+    plot_data_la = bt.band_filter(plot_data_la, np.array(ecg_bandpass), fs_la[type_tag], order=4)
+    plot_data_la, plot_timestamps_la = trim_data(plot_data_la, plot_timestamps_la, fs_la[type_tag], [5/ecg_bandpass[0], 5/ecg_bandpass[0]])
+    ax1.plot(plot_timestamps_la, plot_data_la, color="red")
+    ax1.set_ylabel("Brain Products (ECG)", color="red", fontsize=14)
+    # Detect Peaks
+    peaks_la, _ = scp.signal.find_peaks(plot_data_la, 0.015)
+    ax1.plot(plot_timestamps_la[peaks_la], plot_data_la[peaks_la], "o", color="red")
+    
+    # EmotiBit 
+    type_tag = "PI"    
+    data = []   
+    file_path = file_dir + '\\' + file_base + '\\' + file_base + '_' + type_tag + '.csv'
+    print(file_path)
+    data.append(pd.read_csv(file_path))
+    t = 0
+    plot_timestamps_eb = data[t][timestamp_id].to_numpy()
+    plot_data_eb = -data[t][type_tag]
+    plot_data_eb, plot_timestamps_eb = trim_data(plot_data_eb, plot_timestamps_eb, fs_eb[type_tag], trim_durations) 
+    #plt.plot(plot_timestamps_eb, plot_data_eb, color="black")
+    # Bandpass filter
+    plot_data_eb = bt.band_filter(plot_data_eb, np.array(ppg_bandpass), fs_eb[type_tag], order=4)
+    plot_data_eb, plot_timestamps_eb = trim_data(plot_data_eb, plot_timestamps_eb, fs_eb[type_tag], [5/ppg_bandpass[0], 5/ppg_bandpass[0]]) 
+    ax2.plot(plot_timestamps_eb, plot_data_eb, color="black")
+    # Detect Peaks
+    peaks_eb, _ = scp.signal.find_peaks(plot_data_eb, 75, None, fs_eb[type_tag] / 2)
+    ax2.plot(plot_timestamps_eb[peaks_eb], plot_data_eb[peaks_eb], "o", color="black")
+    ax2.set_ylabel("EmotiBit (PPG)", color="black", fontsize=14)
+    ax1.set_xlabel("Time (seconds)")
+    plt.show()
+    
+    # setup twin axes
+    # Calculate HR
+    # Brain Products
+    type_tag = "ECG"
+    ibis_la = np.diff(peaks_la)/fs_la[type_tag]
+    hr_la = 1 / ibis_la * 60
+    plot_data_la = hr_la
+    plot_timestamps_la = plot_timestamps_la[peaks_la[0:len(peaks_la)-1]]
+    
+    # EmotiBit
+    type_tag = "PI" 
+    ibis_eb = np.diff(peaks_eb)/fs_eb[type_tag]
+    hr_eb = 1 / ibis_eb * 60
+    plot_data_eb = hr_eb
+    plot_timestamps_eb = plot_timestamps_eb[peaks_eb[0:len(peaks_eb)-1]]
+    plt.show()
+    
+    reg_data_x = {}
+    reg_data_x["label"] = "EmotiBit"
+    reg_data_x["data"] = hr_eb
+    reg_data_x["timestamps"] = plot_timestamps_eb
+    
+    reg_data_y = {}
+    reg_data_y["label"] = "Brain Products"
+    reg_data_y["data"] = hr_la
+    reg_data_y["timestamps"] = plot_timestamps_la
+    
+    regression_plots(reg_data_x, reg_data_y, 
+                     title="Heart Rate", xlabel="Time (seconds)", ylabel="Heart Rate (BPM)")
