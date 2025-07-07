1e1f37390e5f33090fecf41104cb39e4fb3b2cf3
jaketheduque <jxie7488@gmail.com>
2025-07-07 12:10:52 -0700

    removed signal.py to fix numpy error and updated README

--- py/examples/scorer_example/hr_scorer_example.py
+++ py/examples/scorer_example/hr_scorer_example.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-from emotibit.scorer import resample, score, plot_both
+from emotibit.scorer import resample, score
 
 """
 In this example detected HR from ECG data and EmotiBit HR data are compared and scored
@@ -11,7 +11,7 @@ def main():
     # first, we need to resample the data, the function will read the files and do that for us
     ebResampled, cyResampled = resample("ebhr6_HR_trim.csv", "LocalTimestamp", "HR", "ecgHR6.csv", "Timestamp", "HR", 100)
     # then, score them
-    slope, intercept, r, rho, tau, p, std_err = score(ebResampled, "HR", cyResampled, "HR", None, "EmotiBit", "Cyton")
+    slope, intercept, r, rho, tau, p, std_err = score(ebResampled, "HR", cyResampled, "HR", "cyton", "EmotiBit", "Cyton", "HR")
     # and print results
     print("Slope: ", slope, "\nIntercept: ", intercept, "\nR: ", r, "\nP: ", p, "\nerr: ", std_err)
 
